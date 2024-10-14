feba4021b1e562db45302ad7920a56a54b0e8b14
Togrul12345 <togrultb-ab205@code.edu.az>
2024-10-14 22:07:36 +0400

    encapsulation task created

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Person
+    {
+        public string _name;
+        public int _age;
+        public DateTime _birthday;
+        public DateTime datetime
+        {
+            get { return _birthday; }
+            set
+            {
+                if (value.Year < 2000)
+                {
+                    _birthday = value;
+                }
+                else
+                {
+                    Console.WriteLine("uygun deyil");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.datetime = new DateTime(2001, 12, 01);
+        }
+    }
+}
